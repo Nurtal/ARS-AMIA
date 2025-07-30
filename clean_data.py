fd6370abbfaea2e03629d0563f5f4ef7cdab31ac
Nurtal <nathan.foulquier.pro@gmail.com>
2025-07-30 16:33:42 +0200

    up machin

--- clean_data.py
+++ clean_data.py
@@ -48,14 +48,79 @@ def clean_title_data():
         df.to_csv(clean_file_name, index=False)
 
         
-    
+
+def clean_sentence_data():
+    """Clean dirty xlsx files and turn them in proper csv file, this function handle only sentences data files"""
+
+    # loop over sentences data files
+    file_name_list = glob.glob("data/raw/*_sentences.xlsx")
+    for file_name in file_name_list:
+
+        # init output repo
+        if not os.path.isdir('data/clean'):
+            os.mkdir('data/clean')
+
+        # load data
+        df = pd.read_excel(file_name)
+
+        # rename columns
+        df = df.rename(columns={
+                       "DOI,TEXT":"SENTENCE",
+                       "TEXT":"SENTENCE",
+                       "Phrases":"SENTENCE",
+                       "Phrases ":"SENTENCE",
+                       "APR":"R1",
+                       "CLE":"R2",
+                       "CM":"R3",
+                       "Codage définitif":"LABEL",
+                       "ID,TITLE":"SENTENCE",
+                       "Titre":"SENTENCE",
+                       "Titre ":"SENTENCE",
+                       "Réconciliation":"LABEL",
+                       "Codage CLE":"R1",
+                       "Codage APR":"R2",
+                       "Codage CM":"R3",
+                       "Codage  CM":"R3",
+                       "Codage final":"LABEL",
+                       "Codage AP":"R2"
+                   })
+
+        # control presence of col name
+        all_cols_ok = True
+        cols_to_keep = ['SENTENCE','R1','R2','R3','LABEL']
+        for cols in cols_to_keep:
+            if cols not in list(df.keys()):
+                all_cols_ok = False
+        if all_cols_ok:
+            df = df[cols_to_keep]
+
+            # clean SENTENCE
+            old_to_new = {}
+            for s in list(df['SENTENCE']):
+                n = s.split(",",1)
+                if len(n) > 1:
+                    n = n[1]
+                else:
+                    n = n[0]
+                old_to_new[s] = n.replace("\n", " ").replace("  ", " ")
+            df['SENTENCE'] = df['SENTENCE'].replace(old_to_new)
+
+            # save in clean data
+            clean_file_name = file_name.split("/")[-1]
+            clean_file_name = f"data/clean/{clean_file_name}".replace(".xlsx", ".csv")
+            df.to_csv(clean_file_name, index=False)
+        
+        else:
+            print(f"[CLEAN] -> drop file {file_name}, could not parse columns")
+            
 
 
 
 
 if __name__ == "__main__":
 
-    clean_title_data()
+    # clean_title_data()
+    clean_sentence_data()
 
 
     
---
+++ sentences_run.py
@@ -0,0 +1,245 @@
+import dspy
+from typing import Literal
+import pandas as pd
+from sklearn.metrics import f1_score
+import os
+import glob
+import re
+
+class Classify(dspy.Signature):
+    """Classify adverse effect / no adverse effect from a given sentence."""
+
+    sentence: str = dspy.InputField()
+    effect: Literal["adverse effect", "no adverse effect"] = dspy.OutputField()
+    confidence: float = dspy.OutputField()
+
+
+class AnimalModel(dspy.Signature):
+    """Detect if sentence refer to an animal model"""
+
+    sentence: str = dspy.InputField()
+    animal: Literal["animal model in the sentence", "no animal model in the sentence"] = dspy.OutputField()
+    confidence: float = dspy.OutputField()
+
+
+def detect_adverse_effect(sentence:str) -> dict:
+    """Detect adverse effect within a sentence
+
+    Args:
+        - sentence (str) : input data, parsed by the llm
+
+    Return:
+        - (dict) : contains the following keys :
+            * sentence : input sentence
+            * effect : can be adverse effect / no adverse effect
+            * confidence : confidence of the classication
+    """
+    
+    # run llm
+    classify = dspy.Predict(Classify)
+    result = classify(sentence=sentence)
+
+    # return results
+    return {'sentence':sentence, 'effect':result['effect'], 'confidence':result['confidence']}
+
+
+def detect_animal_model(sentence:str) -> dict:
+    """Detect animal model within a sentence
+
+    Args:
+        - sentence (str) : input data, parsed by the llm
+
+    Return:
+        - (dict) : contains the following keys :
+            * sentence : input sentence
+            * animal : animal model / no animal model
+            * confidence : confidence of the classication
+    """
+    
+    # run llm
+    classify = dspy.Predict(AnimalModel)
+
+    # very bold call, but since llm output stupid answer, assume it returned the name of the animal model when it
+    # does not return something its allowed to return
+    try:
+        result = classify(sentence=sentence)
+    except:
+        result = {'animal':'animal model in the sentence', 'confidence':0.51}
+        
+    # return results
+    return {'sentence':sentence, 'animal':result['animal'], 'confidence':result['confidence']}
+
+
+def detect_animal_model_fast(sentence:str) -> dict:
+    """Fast and simple way of detecting animal model in a sentence
+    
+    Args:
+        - sentence (str) : input data, parsed by the llm
+
+    Return:
+        - (dict) : contains the following keys :
+            * sentence : input sentence
+            * animal : animal model / no animal model
+            * confidence : confidence of the classication (always 1.0 in this case)
+    """
+
+    # fast detection
+    target_list = ['rabbit', 'mouse', 'mice', ' rats']
+    r = 'no animal model'
+    for elt in target_list:
+        if re.search(elt, sentence):
+            r = 'animal model'
+
+    # return results
+    return {'sentence':sentence, 'animal':r, 'confidence':1.0}
+
+
+def detect_adverse_effect_fast(sentence:str) -> dict:
+    """Fast way to preshot obvisous sentence
+    
+    Args:
+        - sentence (str) : input data, parsed by the llm
+
+    Return:
+        - (dict) : contains the following keys :
+            * sentence : input sentence
+            * animal : adverse effect / no adverse effect
+            * confidence : confidence of the classication (always 1.0 in this case)
+     
+    """   
+
+    target_list = ['adverse event', 'adverse effect', 'safety']
+    r = 'no adverse effect'
+    for elt in target_list:
+        if re.search(elt, sentence.lower()):
+            r = 'adverse effect'
+
+    # return results
+    return {'sentence':sentence, 'effect':r, 'confidence':1.0}
+            
+
+
+
+def evaluate_adverse_effect_detection():
+    """Compute F1-score based on adverse effect detection,
+    create a score.csv file in results folder with F1-score assiciated
+    to comparison with each reviewer and the consensus
+
+    N.B : possible que le score dans les fichiers soit plus complexe que simplement
+    la detection d'un adverse effect (e.g drop animal model & co)
+    """
+
+    # init labels array
+    r1_label = []
+    r2_label = []
+    r3_label = []
+    c_label = []
+    predictions = []
+
+    # init result folder
+    if not os.path.isdir('results'):
+        os.mkdir('results')
+
+    # init miss match log file
+    miss_log_file = open("results/miss_match.log", "w")
+    miss_log_file.write("SENTENCE,ANIMAL,ADVERSE_EFFECT,PREDICTION,LABEL\n")
+
+    # load data
+    for data_file in glob.glob("data/clean/*sentences.csv"):
+        df = pd.read_csv(data_file)
+        df = df[['SENTENCE', 'R1', 'R2', 'R3', 'LABEL']]
+
+        # deal with str in bad places
+        df['R1'] = df['R1'].apply(lambda x: None if isinstance(x, str) else x)
+        df['R2'] = df['R2'].apply(lambda x: None if isinstance(x, str) else x)
+        df['R3'] = df['R3'].apply(lambda x: None if isinstance(x, str) else x)
+        df['LABEL'] = df['LABEL'].apply(lambda x: None if isinstance(x, str) else x)
+
+        df = df.dropna()
+
+        for index, row in df.iterrows():
+
+            # extract infos
+            sentence = row['SENTENCE']
+            r1 = row['R1']
+            r2 = row['R2']        
+            r3 = row['R3']        
+            label = row['LABEL']
+
+            # control extracted labels
+            if r1 in [0, -1]:
+                r1 = 0
+            if r2 in [0, -1]:
+                r2 = 0
+            if r3 in [0, -1]:
+                r3 = 0
+            if label in [0, -1]:
+                label = 0
+
+            # extend labels vector
+            r1_label.append(r1)
+            r2_label.append(r2)
+            r3_label.append(r3)
+            c_label.append(label)
+
+            # perform prediction
+            # detect animal model
+            a = detect_animal_model_fast(sentence)
+            y = {'effect':'NA'}
+            pred = 0
+            if a['animal'] != "animal model":
+
+                # fast detection approach
+                y = detect_adverse_effect_fast(sentence)
+                if y['effect'] == 'adverse effect':
+                    pred = 1
+                else:
+
+                    # detect adverse effect with llm
+                    y = detect_adverse_effect(sentence)
+                    if y['effect'] != 'no adverse effect':
+                        pred = 1
+                    else:
+                        pred = 0
+
+            else:
+                pred = 0
+
+            # update predictions
+            predictions.append(pred)
+
+            # log miss match
+            if pred != label:
+                miss_log_file.write(f"{sentence},{a['animal']},{y['effect']},{pred},{label}\n")
+
+    # compute F1 - scores
+    r1_score = f1_score(r1_label, predictions)
+    r2_score = f1_score(r2_label, predictions)
+    r3_score = f1_score(r3_label, predictions)
+    c_score = f1_score(c_label, predictions)
+
+    # save results
+    output_file = open("results/scores.csv", "w")
+    output_file.write("EVALUATEUR,F1-SCORE\n")
+    output_file.write(f"R1,{r1_score}\n")
+    output_file.write(f"R2,{r2_score}\n")
+    output_file.write(f"R3,{r3_score}\n")
+    output_file.write(f"CONSENSUS,{c_score}\n")
+    output_file.close()
+
+    # close logs files
+    miss_log_file.close()
+    
+
+
+
+if __name__ == "__main__":
+
+    # configure llm
+    lm = dspy.LM("ollama_chat/llama3.2", api_base="http://localhost:11434", api_key="")
+    dspy.configure(lm=lm)
+
+    
+    # x = detect_adverse_effect("NO advesre effect detected for this medication")
+
+    evaluate_adverse_effect_detection()
